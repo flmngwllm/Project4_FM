fa1f5d8e77cef1af479e6d014b09e158ea475b53
Will <flmngwllm232@gmail.com>
2018-12-13 15:26:10 -0500

    making changes

--- filmconnxm/App.js
+++ filmconnxm/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to working </Text>
+        <Text>Open up App.js working </Text>
       </View>
     );
   }
---
+++ filmconnxm/src/Router.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {Scene, Stack, Actions} from "react-native-router-flux";
+import Login from  './components/Login'
+import Home from  './components/Home'
+import Register from  './components/Register'
+import {StyleSheet, StatusBar} from 'react-native'
+
+
+
+class Router extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Router;
